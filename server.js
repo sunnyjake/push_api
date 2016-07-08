bf632fe73d0127621e4fb18686b34542b7671782
sunnyjake <evgenij.jenya@yandex.ru>
2016-07-09 00:19:53 +0300

    updated server and main page

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 var express = require("express");
-app = express();
+
+var app = express();
+
+app.use(express.static(__dirname + "/public"));
+app.use(express.static(__dirname + "/bower_components"));
+
 app.use("/", function(request, response) {
-    console.log("Hey");
-    console.log("new info");
-    console.log("new data");
-    alert("alert");
-});
-app.use("/a", function(request, response){
-    
-});
+    response.sendFile(__dirname.concat("/index.html"));
+})
+.listen(8080);
+
+console.log("Server starts");
